65a583e7ecfd2406cc9170235b519368ae2cef93
Kolotylo Kyrylo <kolot102@gmail.com>
2025-02-25 15:09:13 +0200

    v1 cache

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kiricle/in-memory-cache
+
+go 1.23.5
---
+++ main.go
@@ -0,0 +1,19 @@
+package in_memory_cache
+
+type Cache struct {
+	cache map[string]interface{}
+}
+
+func New() *Cache {
+	return &Cache{
+		cache: make(map[string]interface{}),
+	}
+}
+
+func (c *Cache) Set(key string, value interface{}) {
+	c.cache[key] = value
+}
+
+func (c *Cache) Get(key string) interface{} {
+	return c.cache[key]
+}
